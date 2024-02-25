6e070d93f08153d8607202fb8adcec00c961e1bb
bigboypai <paaai@icliud.com>
2024-02-25 17:58:28 -0600

    Quienes somos page added and styled

--- src/views/secondView.jsx
+++ src/views/secondView.jsx
@@ -3,7 +3,15 @@ import '../styles/secondview.css'
 const secondView = ({ viewRefs }) => {
   return (
     <div className='secondview-container' ref={viewRefs[1]}>
-      Quienes somos
+      <div>
+        <h1> ¿Quienes somos? </h1>
+      </div>
+      <div>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum amet voluptatem nam ut, reiciendis quia obcaecati cum dolorum corrupti repellendus doloribus, culpa esse reprehenderit placeat! Id nulla, provident eaque vero explicabo delectus et officiis labore, ad nihil necessitatibus pariatur commodi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus cum.</p>
+      </div>
+      <div>
+        <h2> PRIDA </h2>
+      </div>
     </div>
   )
 }

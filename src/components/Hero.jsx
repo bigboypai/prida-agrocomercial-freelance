5a2f67d4aaf79344c994e3c3638b20eebb07fb48
bigboypai <paaai@icliud.com>
2024-03-01 15:55:38 -0600

    wireframed ready, sending info to client to make sure it looks good

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
     <div className='hero-container'>
         <div className='hero-content'>
             <h1> TE DAMOS LA BIENVENIDA </h1>
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus alias rem distinctio porro excepturi ratione autem natus error possimus officiis! </p>
-            <button> Learn more </button>
+            <p> Somos apasionados productores y distribuidores de hortalizas y verduras al por mayor. Nos comprometemos a ofrecerte la más alta calidad y frescura en cada uno de nuestros productos, garantizando su inocuidad para un consumo seguro. </p>
+            <button> Contactános </button>
         </div>
         <div className='hero-fake-div'></div>
     </div>
--- src/views/thirdView.jsx
+++ src/views/thirdView.jsx
@@ -2,7 +2,19 @@ import '../styles/thirdView.css'
 const thirdView = ({ viewRefs }) => {
   return (
     <div className='thirdview-container' ref={viewRefs[2]}>
-        Misión / Vision
+        <div>
+          <h2> Nuestra misión </h2>
+          <p> En nuestra empresa, nos comprometemos a ofrecer solidez y confianza a cada uno de nuestros clientes mediante la provisión constante de productos de alta calidad y seguridad alimentaria a lo largo de todo el año. </p>
+        </div>
+
+        <hr />
+
+        <div>
+          <h2> Nuestra visión </h2>
+          <p>
+          Nuestra visión es ser una empresa agrícola productiva e innovadora que expande su línea de productos más allá de las fronteras, con el objetivo de ofrecer calidad y frescura inigualables a nuestros clientes.
+          </p>
+        </div>
     </div>
   )
 }
